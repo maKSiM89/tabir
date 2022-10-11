9d9eec38e7443d87f3bfcb464397bb78caca5f3e
Maksym Abakumov <maksym.abakumov@eleks.com>
2022-10-11 22:10:58 +0300

    Code review changes

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: [ './header.component.scss' ]
 })
 export class HeaderComponent implements OnInit {
+  isMenuShown: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  onBurgerClick(): void {
+    this.isMenuShown = !this.isMenuShown;
+  }
 }
