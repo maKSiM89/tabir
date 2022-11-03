a36717b60321ca254b389f2c9952ae110db6a06c
Kholod-Yura <agenttwixtwix@gmail.com>
2022-11-03 17:39:46 +0200

    add avtoclose burger/changed size font

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -23,12 +23,12 @@ export class HeaderComponent implements OnInit {
 
   onClick(index: number): void {
     this.activeLinkIndex = index;
+    this.isMenuShown = !this.isMenuShown;
   }
 
   @HostListener('window:scroll', [ '$event' ])
   onScroll(event: any): void {
     const scrollTop: number = event.target.scrollingElement.scrollTop;
-
     this.isHeaderVisible = scrollTop < this.headerHeight;
   }
 }
